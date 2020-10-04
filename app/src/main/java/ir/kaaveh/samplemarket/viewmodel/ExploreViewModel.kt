8df5ba8bc4f67b361944ee0b2cb4ea39e33e1c33
Kaaveh <Kaaveh@protonmail.com>
2020-10-04 21:05:40 +0330

    Complete viewmodel

--- app/src/main/java/ir/kaaveh/samplemarket/viewmodel/ExploreViewModel.kt
+++ app/src/main/java/ir/kaaveh/samplemarket/viewmodel/ExploreViewModel.kt
@@ -1,4 +1,20 @@
 package ir.kaaveh.samplemarket.viewmodel
 
-class ExploreViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import ir.kaaveh.samplemarket.model.Item
+import ir.kaaveh.samplemarket.repository.ItemRepository
+
+class ExploreViewModel(itemRepository: ItemRepository) : ViewModel() {
+    private val _itemList = MutableLiveData<List<Item>>()
+
+    val itemList: LiveData<List<Item>>
+        get() = _itemList
+
+    init {
+        itemRepository.itemList.observeForever {
+            _itemList.postValue(it)
+        }
+    }
 }
--- app/src/main/java/ir/kaaveh/samplemarket/viewmodel/ExploreViewModelFactory.kt
+++ app/src/main/java/ir/kaaveh/samplemarket/viewmodel/ExploreViewModelFactory.kt
@@ -1,4 +1,12 @@
 package ir.kaaveh.samplemarket.viewmodel
 
-class ExploreViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import ir.kaaveh.samplemarket.repository.ItemRepository
+
+class ExploreViewModelFactory(private val repository: ItemRepository) :
+    ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return modelClass.getConstructor(ItemRepository::class.java).newInstance(repository)
+    }
 }
