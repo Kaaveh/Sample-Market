1ed5fc9ea7cd3d2bee8d91e021032c2baa754930
Kaaveh <Kaaveh@protonmail.com>
2020-10-05 10:24:26 +0330

    Add README.md

---
+++ README.md
@@ -0,0 +1,17 @@
+# Sample Market
+
+This is my test for interview with Pichas Co.
+
+Using:
+- Kotlin
+- MVVM architectural pattern
+- lifecycle
+- LiveData
+- RecyclerView
+- Material design
+- Navigation
+- BottomNavigationView
+
+# Screenshot
+
+<img src="screenshots/1.png" width="250"/>
--- app/src/main/java/ir/kaaveh/samplemarket/view/MainActivity.kt
+++ app/src/main/java/ir/kaaveh/samplemarket/view/MainActivity.kt
@@ -3,7 +3,9 @@ package ir.kaaveh.samplemarket.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import ir.kaaveh.samplemarket.R
 import ir.kaaveh.samplemarket.databinding.ActivityMainBinding
@@ -15,7 +17,9 @@ class MainActivity : AppCompatActivity() {
         val binding =
             DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
-        val navController = findNavController(binding.navHostFragment.id)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
         binding.bottomNav.setupWithNavController(navController)
     }
 }
